564d84265d1cae0792abd8f5ed0d2f7c4216b5c5
busma13 <peterluitjens@gmail.com>
2022-04-16 09:50:34 -0700

    add isThereAVowelInThere.js

--- arrayPlusArray.js
+++ arrayPlusArray.js
@@ -7,4 +7,6 @@ P.S. Each array includes only integer numbers. Output is a number too.
 
 function arrayPlusArray(arr1, arr2) {
     return arr1.reduce((acc, c) => acc + c, 0) + arr2.reduce((acc, c) => acc + c, 0);
-  }
+}
+
+  
