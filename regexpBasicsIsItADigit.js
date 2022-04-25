4b1e54fb0a236575da4a924afbf87cb18ceceb9f
busma13 <peterluitjens@gmail.com>
2022-04-24 19:17:29 -0700

    add regexpBasicsIsItADigit.js

---
+++ regexpBasicsIsItADigit.js
@@ -0,0 +1,12 @@
+/* Regexp Basics - is it a digit?
+
+Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+*/
+
+String.prototype.digit = function(s) {
+    if (this.length > 1) {
+        return false;
+    } else {
+        return /\d/.test(this);
+    }
+};
