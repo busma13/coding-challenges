1a2dabac4bbe85850d9a1eae2c0a59889cb1e09d
busma13 <peterluitjens@gmail.com>
2022-05-13 06:06:28 -0700

    add convertABooleanToAString.js

---
+++ convertABooleanToAString.js
@@ -0,0 +1,18 @@
+/* Convert A Boolean To A String
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+//PREP
+//Parameters: a boolean value
+//Returns: a string value representing the boolean
+//Examples booleanToString(true)//'true', booleanToString(false)//'false'
+//Pseudo code: see comments in booleanToString()
+
+function booleanToString(b){
+    //convert b to its string value
+    //return b
+    return b.toString();
+}
