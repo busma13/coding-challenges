92fd52d8bcd2ec98f914a99a9b3e7d5cb47f74d5
busma13 <peterluitjens@gmail.com>
2022-05-27 06:10:48 -0700

    add alanPartridgeII-AppleTurnover.js

---
+++ alanPartridgeII-AppleTurnover.js
@@ -0,0 +1,38 @@
+/* Alan Partridge II - Apple Turnover
+As a treat, I'll let you read part of the script from a classic 'I'm Alan Partridge episode:
+
+Lynn: Alan, there’s that teacher chap.
+Alan: Michael, if he hits me, will you hit him first?
+Michael: No, he’s a customer. I cannot hit customers. I’ve been told. I’ll go and get some stock.
+Alan: Yeah, chicken stock.
+Phil: Hello Alan.
+Alan: Lynn, hand me an apple pie. And remove yourself from the theatre of conflict.
+Lynn: What do you mean?
+Alan: Go and stand by the yakults. The temperature inside this apple turnover is 1,000 degrees. If I squeeze it, a jet of molten Bramley apple is going to squirt out. Could go your way, could go mine. Either way, one of us is going down.
+
+Alan is known for referring to the temperature of the apple turnover as 'Hotter than the sun!'. According to space.com the temperature of the sun's corona is 2,000,000 degrees C, but we will ignore the science for now.
+
+Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+
+X will be a valid integer number.
+
+X will be either a number or a string. Both are valid. 
+
+PREP
+Parameters: A number or a string whos value is a valid integer number.
+Returns: One of two strings: 'It's hotter than the sun!!' or 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+Examples: apple(5); // 'Help yourself to a honeycomb Yorkie for the glovebox.'
+          apple('50'); // 'It's hotter than the sun!!'
+Pseudo code: Convert x to a number.
+             Square x.
+             Compare that value to 1000.
+             Return one of the two strings depending on the result.
+*/
+
+function apple(x){
+    return (Number(x)**2 > 1000) ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' 
+}
+
+console.log(apple('5'))
+console.log(apple(5))
+console.log(apple('50'))
