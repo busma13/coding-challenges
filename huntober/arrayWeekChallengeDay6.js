334303e2ba84a078bf9267b7c667c1412f4162fe
busma13 <peterluitjens@gmail.com>
2022-10-16 14:01:35 -0700

    Add arrayWeekChallengeDay6.js

---
+++ huntober/arrayWeekChallengeDay6.js
@@ -0,0 +1,25 @@
+/* Array Week Challenge - Day 6
+
+This week has only 6 days of challenges, and this is day 6. Ready to mix it up and put those array functions to good use? I hope so!
+
+Today you'll be plugging in your functions to a skeleton react project to create a sliding puzzle game. Remember those things? I've added comments to the project Codepen, but before you click over there let's go over some key tasks and notes.
+
+Here is the Codepen for the project: https://codepen.io/blawblawLaw/pen/xxjMNrN
+
+You'll see 3 files: html, css, and js already over there. Please do the following:
+
+    Fork the Codepen if it didn't automatically
+    Add a link to your Github or Twitter to the footer in the html file (I know Codepen outside links don't work, but humor me)
+    Insert your daily functions where noted in the js file
+        Note that they need specific names and parameters
+        Remember to insert any noted code from the comment within the functions
+    Play your game! Are there bugs? No worries, debug away
+
+This is my first time having people blind-build functions to plug into a project, so if you get stuck feel free to reach out! It could be that I need to modify the instructions.
+
+I want to point out that I did call it a skeleton project because it is! Feel free to make it your own and add styles or content. Personally, I want to investigate sprites and try that out for the individual tiles so that I can easily use any square image and have a grid size selector.
+
+
+https://codepen.io/busma13/pen/VwxNvLa
+
+*/
