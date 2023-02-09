73e3581924ec7b76191d4529c8aa62d92b238a3c
busma13 <peterluitjens@gmail.com>
2023-02-09 05:51:20 -0700

    Add EvenOrOdd.java

---
+++ javaProblems/codewars/EvenOrOdd.java
@@ -0,0 +1,14 @@
+package javaProblems.codewars;
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        //Place code here
+        return (number % 2 == 0) ? "Even" : "Odd";
+    }
+    public static void main(String[] args) {
+        System.out.println(even_or_odd(6));    
+        System.out.println(even_or_odd(7));    
+        System.out.println(even_or_odd(0));    
+        System.out.println(even_or_odd(-1));    
+    }
+}
