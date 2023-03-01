7a0b8d529aaad810cbee6a6f9839b87ccf0a6485
busma13 <peterluitjens@gmail.com>
2023-03-01 05:47:19 -0700

    Add ReturnNegative.java

---
+++ javaProblems/codewars/ReturnNegative.java
@@ -0,0 +1,14 @@
+package javaProblems.codewars;
+
+public class ReturnNegative {
+    public static int makeNegative(final int x) {
+
+        return x < 0 ? x : -x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makeNegative(1));
+        System.out.println(makeNegative(-5));
+        System.out.println(makeNegative(0));
+    }
+}
